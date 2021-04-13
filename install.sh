61ba4c8091a8eae21438d3591e89d15953fc05ea
Michael Kalantar <kalantar@us.ibm.com>
2021-04-13 15:21:28 -0400

    istio domain and restructure stacks

--- install.sh
+++ install.sh
@@ -8,8 +8,11 @@ export TAG="${TAG:-v0.3.0}"
 # Step 1: Install Iter8
 echo "Installing Iter8"
 kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-install/${TAG}/core/build.yaml
-kubectl wait crd -l creator=iter8 --for condition=established --timeout=120s
-kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-install/${TAG}/metrics/build.yaml
+
+# Step 2: Install stacks
+# By default, all are installed. 
+# To install a single stack or a subset of the stacks, use kustomize
+kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-install/${TAG}/stacks/build.yaml
 
 echo "Verifying Iter8 installation"
 kubectl wait --for condition=ready --timeout=300s pods --all -n iter8-system
