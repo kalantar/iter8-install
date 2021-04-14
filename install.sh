abcfb85d83d93e04b8478ddd80a0d8128195b79a
Michael Kalantar <kalantar@us.ibm.com>
2021-04-14 09:36:49 -0400

    introduce GIT_ACCOUNT

--- install.sh
+++ install.sh
@@ -2,17 +2,18 @@
 
 set -e
 
-# Step 0: Export TAG
+# Step 0: Export GIT_ACCOUNT and TAG
+export GIT_ACCOUNT="${GIT_ACCOUNT:iter8-tools}"
 export TAG="${TAG:-v0.3.0}"
 
 # Step 1: Install Iter8
 echo "Installing Iter8"
-kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-install/${TAG}/core/build.yaml
+kubectl apply -f https://raw.githubusercontent.com/${GIT_ACCOUNT}/iter8-install/${TAG}/core/build.yaml
 
 # Step 2: Install stacks
 # By default, all are installed. 
 # To install a single stack or a subset of the stacks, use kustomize
-kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-install/${TAG}/stacks/build.yaml
+kubectl apply -f https://raw.githubusercontent.com/${GIT_ACCOUNT}/iter8-install/${TAG}/stacks/build.yaml
 
 echo "Verifying Iter8 installation"
 kubectl wait --for condition=ready --timeout=300s pods --all -n iter8-system
